392dbf44f31bfd78bcea8a4cdf0c1f1758515977
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2022-02-15 14:47:34 -0800

    Added particles
    - added particle effect to hit notes

--- 151AudioReactive/Assets/Scripts/spawnedObject.cs
+++ 151AudioReactive/Assets/Scripts/spawnedObject.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class spawnedObject : MonoBehaviour
 {
+    public ParticleSystem hitParticles;
     private GameObject objectToMoveTo;
     private GameManagerScript GM;
     private float speed = .01f;
     private float startTime;
     private float tripLength;
     private bool canPlayerHit = false;
-    // Start is called before the first frame update
+
+
     void Start()
     {
         // acquiring GameplayManager
@@ -32,15 +34,20 @@ public class spawnedObject : MonoBehaviour
 
         transform.position = Vector3.Lerp(transform.position, objectToMoveTo.transform.position, partOfTrip);
 
+        // can add && !GM.hit to this statment to only allow one note to be hit per press,
+        // can't specify which note (maybe add a variable keeping track of spawning order?)
         if(canPlayerHit && Input.GetKeyDown(KeyCode.Space)){
             print("HIT");
             GM.incrementScore(1 * GM.multiplier);
             GM.incrementMultiplier(1);
             GM.hit = true;
             Destroy(gameObject);
+            ParticleSystem particle = Instantiate(hitParticles, this.transform.position, Quaternion.identity);
+            Destroy(particle, 1);
+
         } 
 
-        // this should probably be integrated into the other if statement, but it'd be confusing and I'm lazy
+        // this should probably be integrated into the above if statement, but  I'm lazy
         if (Input.GetKeyDown(KeyCode.Space) && !canPlayerHit)
         {
             GM.attemptedHit = true;
